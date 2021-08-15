39d6c680e6d7a2ce1cfe80c19b16fde8254b0e1e
rsshonjoydas <rsshonjoydas@gmail.com>
2021-08-15 14:40:23 +0600

    axios instances

--- main.js
+++ main.js
@@ -166,7 +166,12 @@ axios.interceptors.request.use(
   }
 );
 
-// AXIOS INSTANCES
+//! AXIOS INSTANCES
+const axiosInstance = axios.create({
+  //? Other custom settings
+  baseURL: 'https://jsonplaceholder.typicode.com'
+});
+axiosInstance.get('/comments').then(res => showOutput(res))
 
 // Show output in browser
 function showOutput(res) {
